9fe37f7efdbb233db96a3eff08bd4b389a072e41
Jesse Akorah <jesse@Jesses-MBP.lan>
2022-05-09 02:13:02 +0100

    Update 'Checkout' file name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Checkout from "./ Checkout";
+import Checkout from "./Checkout";
 
 function App() {
   return (
